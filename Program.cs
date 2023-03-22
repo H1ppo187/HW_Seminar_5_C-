578dbb1efeae7f353ce8da43db88fc7d2ede0f61
Alexander Balashov <balashov187@yandex.ru>
2023-03-22 21:25:34 +0300

    Задача 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[]array = GetRandomArray(10, 100, 999);
+Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine(FindElement(array));
+
+// -------------------------------Общий метод-------------------------------------------
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+// -------------------------------Общий метод-------------------------------------------
+int FindElement(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el % 2 == 0) count++;
+    }
+    return count;
+}
