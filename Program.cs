110048aeb5781fd746871ef0c73b940b6e56f884
Alexander Balashov <balashov187@yandex.ru>
2023-03-22 21:45:33 +0300

    Задача 2

--- Program.cs
+++ Program.cs
@@ -2,9 +2,42 @@
 
 // [345, 897, 568, 234] -> 2
 
-int[]array = GetRandomArray(10, 100, 999);
+// int[]array = GetRandomArray(10, 100, 999);
+// Console.WriteLine($"[{String.Join(",", array)}]");
+// Console.WriteLine(FindElement(array));
+
+// // -------------------------------Общий метод-------------------------------------------
+// int[] GetRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+
+//     return result;
+// }
+
+// // -------------------------------Общий метод-------------------------------------------
+// int FindElement(int[] array)
+// {
+//     int count = 0;
+//     foreach (int el in array)
+//     {
+//         if (el % 2 == 0) count++;
+//     }
+//     return count;
+// }
+
+// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[]array = GetRandomArray(10, 0, 99);
 Console.WriteLine($"[{String.Join(",", array)}]");
-Console.WriteLine(FindElement(array));
+Console.WriteLine(SumElement(array));
 
 // -------------------------------Общий метод-------------------------------------------
 int[] GetRandomArray(int size, int minValue, int maxValue)
@@ -19,12 +52,14 @@ int[] GetRandomArray(int size, int minValue, int maxValue)
 }
 
 // -------------------------------Общий метод-------------------------------------------
-int FindElement(int[] array)
+int SumElement(int[] array)
 {
-    int count = 0;
-    foreach (int el in array)
+    int sum = 0;
+    int i = 1;
+    while (i <= array.Length)
     {
-        if (el % 2 == 0) count++;
+        sum += array[i];
+        i += 2;
     }
-    return count;
+    return sum;
 }
